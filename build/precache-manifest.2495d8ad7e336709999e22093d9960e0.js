244f8cdf84f6a4f29e6748a32bd4a15b99097b08
Rahul Jaglan <54793387+rahuljaglan@users.noreply.github.com>
2021-11-30 00:54:42 +0530

    Add files via upload

---
+++ build/precache-manifest.2495d8ad7e336709999e22093d9960e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987d77278ef90fb7985344a1b7148595",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8761d7ebaa978c364b4",
+    "url": "./static/css/2.80dce9aa.chunk.css"
+  },
+  {
+    "revision": "d6a19bc34a8d376f6c0c",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "f8761d7ebaa978c364b4",
+    "url": "./static/js/2.81fee5a4.chunk.js"
+  },
+  {
+    "revision": "30545a24812bc2557c4d652dede4cf2a",
+    "url": "./static/js/2.81fee5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a19bc34a8d376f6c0c",
+    "url": "./static/js/main.0ff1da54.chunk.js"
+  },
+  {
+    "revision": "fc71048510e0974eab3b",
+    "url": "./static/js/runtime-main.be13530a.js"
+  },
+  {
+    "revision": "9e02a88e54fb14899a2ea3c60bf8fa35",
+    "url": "./static/media/dvideo.9e02a88e.png"
+  }
+]);
